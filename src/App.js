353cc63113c55cdc9008e3379d829e8e9f3a6c48
Amy S <pbrmajhar@gmail.com>
2021-08-11 21:27:57 +0600

    all token and token preview are added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import "./App.css";
+
+//importing components
+import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
+import Newtoken from "./pages/Newtoken";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Switch>
+        <Route path="/" exact component={Login} />
+        <Route path="/dashboard" exact component={Dashboard} />
+        <Route path="/newtoken" exact component={Newtoken} />
+      </Switch>
+    </>
   );
 }
 
